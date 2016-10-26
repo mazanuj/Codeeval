e6c5881d12a984473f48433859456c2908e78481
Oleksandr Mazyaka <mazanuj@gmail.com>
2016-10-26 14:13:35 +0300

    FIRST NON-REPEATED CHARACTER

---
+++ 010 MTH TO LAST ELEMENT/Program.cs
@@ -0,0 +1,37 @@
+﻿//https://www.codeeval.com/open_challenges/10/
+//Write a program which determines the Mth to the last element in a list.
+//INPUT SAMPLE:
+//The first argument is a path to a file.The file contains the series of space delimited characters followed by an integer.The integer represents an index in the list(1-based), one per line.
+//For example:
+//a b c d 4
+//e f g h 2
+//OUTPUT SAMPLE:
+//Print to stdout the Mth element from the end of the list, one per line.If the index is larger than the number of elements in the list, ignore that input.
+//For example:
+//a
+//g
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _010_MTH_TO_LAST_ELEMENT
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            var arr = File.ReadAllLines(args[0]).Select(x => x.Split(' '));
+            foreach (var list in arr.Select(x => new List<string>(x.Reverse())))
+            {
+                var m = int.Parse(list[0]);
+                list.Remove(list[0]);
+                if (m <= list.Count)
+                    Console.WriteLine(list[m - 1]);
+            }
+
+            return 0;
+        }
+    }
+}
---
+++ 012 FIRST NON-REPEATED CHARACTER/Program.cs
@@ -0,0 +1,31 @@
+﻿//https://www.codeeval.com/open_challenges/12/
+//Write a program which finds the first non-repeated character in a string.
+//INPUT SAMPLE:
+//The first argument is a path to a file.The file contains strings.
+//For example:
+//yellow
+//tooth
+//OUTPUT SAMPLE:
+//Print to stdout the first non-repeated character, one per line.
+//For example:
+//y
+//h
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace _012_FIRST_NON_REPEATED_CHARACTER
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            var arr = File.ReadAllLines(args[0]);
+            foreach (var firstChar in arr.Select(list => list.GroupBy(x => x).First(x => x.Count() == 1).Key))
+                Console.WriteLine(firstChar);
+
+            return 0;
+        }
+    }
+}
