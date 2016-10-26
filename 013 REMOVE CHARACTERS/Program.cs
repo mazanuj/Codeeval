d62d3273001b8c10b3bd9260b457494494daa593
Oleksandr Mazyaka <mazanuj@gmail.com>
2016-10-27 01:06:32 +0300

    STRING PERMUTATIONS

---
+++ 013 REMOVE CHARACTERS/Program.cs
@@ -0,0 +1,34 @@
+﻿//https://www.codeeval.com/open_challenges/13/
+//Write a program which removes specific characters from a string.
+//INPUT SAMPLE:
+//The first argument is a path to a file.The file contains the source strings and the characters that need to be scrubbed.Each source string and characters you need to scrub are delimited by comma.
+//For example:
+//how are you, abc
+//hello world, def
+//OUTPUT SAMPLE:
+//Print to stdout the scrubbed strings, one per line.Ensure that there are no trailing empty spaces on each line you print.
+//For example:
+//how re you
+//hllo worl
+
+using System;
+using System.IO;
+using System.Linq;
+
+namespace _013_REMOVE_CHARACTERS
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            var arr = File.ReadAllLines(args[0]).Select(x => x.Split(new[] {", "}, StringSplitOptions.None));
+            foreach (var output in from list in arr
+                let output = list.First()
+                let scr = list.Last().ToList()
+                select scr.Aggregate(output, (current, s) => current.Replace(s.ToString(), "")))
+                Console.WriteLine(output);
+
+            return 0;
+        }
+    }
+}
---
+++ 014 STRING PERMUTATIONS/Program.cs
@@ -0,0 +1,61 @@
+﻿//https://www.codeeval.com/open_challenges/14/
+//Write a program which prints all the permutations of a string in alphabetical order. We consider that digits < upper case letters < lower case letters. The sorting should be performed in ascending order.
+//INPUT SAMPLE:
+//Your program should accept a file as its first argument.The file contains input strings, one per line.
+//For example:
+//hat
+//abc
+//Zu6
+//OUTPUT SAMPLE:
+//Print to stdout the permutations of the string separated by comma, in alphabetical order.
+//For example:
+//aht,ath,hat,hta,tah,tha
+//abc,acb,bac,bca,cab,cba
+//6Zu,6uZ,Z6u,Zu6,u6Z,uZ6
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _014_STRING_PERMUTATIONS
+{
+    public static class Program
+    {
+        public static int Main(string[] args)
+        {
+            var arr = File.ReadAllLines(args[0]);
+            foreach (var perm in arr.Select(GetPermutations))
+                Console.WriteLine(string.Join(",", perm));
+
+            return 0;
+        }
+
+        private static IEnumerable<string> GetPermutations(string word)
+        {
+            if (word.Length == 2)
+            {
+                var c = word.ToCharArray();
+                var s = new string(new[] {c[1], c[0]});
+                return new[] {word, s};
+            }
+            var result = new List<string>();
+            var subsetPermutations = GetPermutations(word.Substring(1));
+            var firstChar = word[0];
+            foreach (var temp in subsetPermutations.Select(s => firstChar + s))
+            {
+                result.Add(temp);
+                var chars = temp.ToCharArray();
+                for (var i = 0; i < temp.Length - 1; i++)
+                {
+                    var t = chars[i];
+                    chars[i] = chars[i + 1];
+                    chars[i + 1] = t;
+                    var s2 = new string(chars);
+                    result.Add(s2);
+                }
+            }
+            return result.OrderBy(x => x, StringComparer.Ordinal);
+        }
+    }
+}
